1e0608faeb69ae6cce7d1609ff8cdb7177f873e0
aviadc <coaviadco@gmail.com>
2022-06-08 00:32:56 +0300

    about

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -4,7 +4,16 @@ import "./about.scss"
 const About = () => {
   return (
     <div className='about-container' id='about'>
-      <h2>About</h2> 
+      <div className='about-left'>
+        <div className='about-image'>
+
+        </div>
+        <div className='about-description'>
+          <h2>who I'm am</h2>
+        </div>
+        </div> 
+      <div className='about-right'>
+        </div> 
       </div>
   )
 }
