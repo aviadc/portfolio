085a81184cb95278373d21d16ea2362bfb6ee3d1
aviadc <coaviadco@gmail.com>
2022-06-10 18:25:25 +0300

    scroll animation

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 import SkillRange from '../../SkillRange'
 import "./about.scss"
 
 const About = () => {
+  useEffect(()=>{
+    // Aos.init({duration: 2000});
+  },[])
   return (
-    <div className='about-container' id='about'>
+    <div className='about-container' id='about' >
       <div className='about-inner'>
         <div className='about-left'>
           <div className='about-image'>
@@ -20,7 +25,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className='about-right'>
+        <div className='about-right' >
           <SkillRange size='85' delay='0' />
           <SkillRange size='75' delay='.1' />
           <SkillRange size='90' delay='.2' />
