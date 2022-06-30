47cc961ab9fe0700823bfcf6414720e831d4e32f
aviadc <coaviadco@gmail.com>
2022-06-30 14:45:08 +0300

    emailjs

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import PageTitle from '../../PageTitle'
-import "./contact.scss"
+import React from "react";
+import emailjs from "@emailjs/browser";
+import PageTitle from "../../PageTitle";
+import "./contact.scss";
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm("service_u2r26pi", "template_pxi3mti", e.target,'SU6sZXbXeQVKcGJSw')
+    .then(res=>{console.log(res)}).catch(err=>{
+      console.log(err)
+    });
+  };
+
   return (
     <div className='contact-container' id='contact'>
       <PageTitle title='Contact' />
-    <form className='contact-form' >
-      <input type='text' name='name' placeholder='Name' />
-      <input type='email' name='email' placeholder='Enter email' />
-      <textarea name="message" placeholder='Your message' />
-      <button>SUBMIT</button>
-    </form>
+      <form className='contact-form' onSubmit={handleSubmit}>
+        <input type='text' name='name' placeholder='Name' />
+        <input type='email' name='email' placeholder='Enter email' />
+        <textarea name='message' placeholder='Your message' />
+        <button>SUBMIT</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
