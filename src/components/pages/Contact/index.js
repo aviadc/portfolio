5031dd8ef7665848d328985e0ce369065187456c
aviadc <coaviadco@gmail.com>
2022-07-01 10:33:03 +0300

    contact form validation and css

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React ,{useState} from "react";
 import emailjs from "@emailjs/browser";
 import PageTitle from "../../PageTitle";
 import "./contact.scss";
 
 const Contact = () => {
+
+  const [messageToUser,setMessageToUser] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!(e.target[0].value&&e.target[1].value&&e.target[2].value)){ //check if the input fields not empty
+      setMessageToUser("missing details!")
+      return;
+    }
+    e.target.reset();
     emailjs.sendForm("service_u2r26pi", "template_pxi3mti", e.target,'SU6sZXbXeQVKcGJSw')
-    .then(res=>{console.log(res)}).catch(err=>{
-      console.log(err)
+    .then(res=>{
+      console.log(res);
+      setMessageToUser("Your message has been sent successfully");
+    }).catch(err=>{
+      console.log(err);
+      setMessageToUser("There was an error trying to send your message, please try again later.");
     });
   };
+
+  
 
   return (
     <div className='contact-container' id='contact'>
@@ -18,9 +32,10 @@ const Contact = () => {
       <form className='contact-form' onSubmit={handleSubmit}>
         <input type='text' name='name' placeholder='Name' />
         <input type='email' name='email' placeholder='Enter email' />
-        <textarea name='message' placeholder='Your message' />
+        <textarea name='message' placeholder='Your message' rows='10' />
         <button>SUBMIT</button>
       </form>
+      <h5>{messageToUser}</h5>
     </div>
   );
 };
