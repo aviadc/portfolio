35e34077c105f669bd965a2061f24425158d0930
aviadc <coaviadco@gmail.com>
2022-07-04 15:43:53 +0300

    skill

--- src/components/SkillItem/index.js
+++ src/components/SkillItem/index.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import StyledSkillRange from '../styledComponents/StyledSkillRange'
-import "./skillItem.scss"
+import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import "./skillItem.scss";
 
 const SkillItem = () => {
   return (
     <div className='skill-container'>
-      <div className='skill-name'>
-        
+      <div className='skill-left'>
+        <FaHtml5 />
       </div>
-      <div className='skill-range-bar'>
-       
-        <h4>%</h4>
+      <div className='skill-right'>
+        <div className='skill-name'>HTML</div>
+        <div className='skill-bar'>
+          <div className='skill-progress'></div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SkillItem
+export default SkillItem;
--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -26,12 +26,12 @@ const About = () => {
         <div className='about-right'>
           <div className='skill-range-container'>
             <SkillItem size='85' delay='0' />
-            <SkillItem size='75' delay='.1' />
+            {/* <SkillItem size='75' delay='.1' />
             <SkillItem size='90' delay='.2' />
             <SkillItem size='65' delay='.3' />
             <SkillItem size='95' delay='.4' />
             <SkillItem size='70' delay='.5' />
-            <SkillItem size='80' delay='.6' />
+            <SkillItem size='80' delay='.6' /> */}
           </div>
         </div>
       </div>
