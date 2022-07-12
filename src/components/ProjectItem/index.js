c9550af4d90c0b6df985b9ed0d3ed0f23be56173
aviadc <coaviadco@gmail.com>
2022-07-12 18:15:40 +0300

    project item

---
+++ src/components/ProjectItem/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./projectItem.scss"
+
+const ProjectItem = (props) => {
+  return (
+    <div className='project-item-container'>
+      <button className='button-code'>see code</button>
+      <div className='project-item-inner'>
+        <img src={props.img} alt='project' className='ff' />
+      </div>
+      <button className='button-website'>go to website</button>
+    </div>
+  )
+}
+
+export default ProjectItem
--- src/components/pages/Work/index.js
+++ src/components/pages/Work/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PageTitle from "../../PageTitle"
+import ProjectItem from "../../ProjectItem"
+import me2 from "../../../assets/me2.jpeg"
 import "./work.scss"
 
 const Work = () => {
@@ -7,7 +9,11 @@ const Work = () => {
     <div className='work-page' id='work'>
       <PageTitle title='Projects' style={{color: "black"}} />
       <div className='work-container'>
-
+        <ProjectItem img ={me2}/>
+        <ProjectItem img ={me2}/>
+        <ProjectItem img ={me2}/>
+        <ProjectItem img ={me2}/>
+        
       </div>
     </div>
   )
