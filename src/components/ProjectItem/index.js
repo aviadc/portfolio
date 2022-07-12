bf079200c0a32b847466370432a0b670fff88032
aviadc <coaviadco@gmail.com>
2022-07-13 00:31:45 +0300

    project button

--- src/components/ProjectItem/index.js
+++ src/components/ProjectItem/index.js
@@ -6,7 +6,7 @@ const ProjectItem = (props) => {
     <div className='project-item-container'>
       <button className='button-code'>see code</button>
       <div className='project-item-inner'>
-        <img src={props.img} alt='project' className='ff' />
+        <img src={props.img} alt='project' />
       </div>
       <button className='button-website'>go to website</button>
     </div>
