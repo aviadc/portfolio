227a9fb8ff4c4003958040b135ecb7fd89608545
aviadc <coaviadco@gmail.com>
2022-07-13 18:16:34 +0300

    video home background

--- src/components/ParticlesIntro/index.js
+++ src/components/ParticlesIntro/index.js
@@ -18,86 +18,25 @@ const ParticlesIntro = () => {
   return (
     <div className="particles-container">
       <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        canvasClassName="particle-canvas"
-        options={{
-          fullScreen: {
-            enable: true
-          },
-          detectRetina: true,
-          background: {
-            color: "#000"
-          },
-          fpsLimit: 60,
-          emitters: {
-            direction: "top",
-            life: {
-              count: 0,
-              duration: 0.08,
-              delay: 0.1
-            },
-            rate: {
-              delay: 0.15,
-              quantity: 1
-            },
-            size: {
-              width: 100,
-              height: 0
-            },
-            position: {
-              y: 100,
-              x: 50
-            }
-          },
-          particles: {
-            number: {
-              value: 0
-            },
-            life: {
-              count: 5
-            },
-            shape: {
-              type: "line"
-            },
-            size: {
-              value: 50,
-              animation: {
-                enable: true,
-                sync: true,
-                speed: 1,
-                startValue: "max",
-                destroy: "min"
-              }
-            },
-            stroke: {
-              color: {
-                value: "#2BFFE6"
+        params={{
+          "particles": {
+              "number": {
+                  "value": 50
               },
-              width: 1
-            },
-            rotate: {
-              path: true,
-              value: 180
-            },
-            move: {
-              straight: true,
-              enable: true,
-              speed: { min: 10, max: 20 },
-              outModes: {
-                default: "destroy",
-                top: "none"
-              },
-              trail: {
-                fillColor: "#000",
-                enable: true,
-                length: 3
+              "size": {
+                  "value": 3
               }
-            }
+          },
+          "interactivity": {
+              "events": {
+                  "onhover": {
+                      "enable": true,
+                      "mode": "repulse"
+                  }
+              }
           }
-        }}
-      />
+      }} />
+      
     </div>
   );
 };
--- src/components/pages/Intro/index.js
+++ src/components/pages/Intro/index.js
@@ -1,6 +1,7 @@
-import React , {useState} from 'react'
+import React from 'react'
 import ParticlesIntro from '../../ParticlesIntro'
-import { FiArrowRight , FiArrowDown } from 'react-icons/fi';
+import { FiArrowRight  } from 'react-icons/fi'
+import videoBg from "../../../assets/videos/background-video.mp4"
 
 import "./intro.scss"
 
@@ -9,16 +10,14 @@ const Intro = () => {
 
   return (
     <>
-      <div className='particles-intro'>
-        <ParticlesIntro />
-      </div>
       <div className='intro-container' id='intro'>
+        <video className='video-bg' autoPlay muted loop>
+          <source src={videoBg} type='video/mp4' />
+        </video>
         <div className='intro-inner'>
           <div>Hello, I'm  <span>Aviad Cohen.</span></div>
           <div>I'm a Full-Stack Web Developer.</div>
          <a href='#work'> <div className='intro-mywork-button'>
-             {/* onMouseEnter={() => setIsHover(true)}
-             onMouseLeave={() => setIsHover(false)}> */}
           view my work <span><FiArrowRight /></span></div> </a>
         </div>
       </div>
